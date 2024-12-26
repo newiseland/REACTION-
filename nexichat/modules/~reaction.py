1f747b386c7351e6029b82e5c91b8fe05694fb3f
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-26 19:36:05 +0530

    Update ~reaction.py

--- nexichat/modules/~reaction.py
+++ nexichat/modules/~reaction.py
@@ -1,10 +1,15 @@
+
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from nexichat import nexichat
 
 @nexichat.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
+    emojis = ["👍", "❤️", "😂", "🔥", "😎", "💯", "👏", "😊", "🎉", "🙌", 
+              "🤔", "😅", "😢", "😡", "🤩", "🎶", "✅", "🌟", "👀", "💪"]
     try:
-        await message.react("👍, ❤️, 💀,💥")
+        for emoji in emojis:
+            await message.react(emoji)
     except Exception as e:
         print(f"Failed to react to message: {e}")
