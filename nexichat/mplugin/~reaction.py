f91bc3b56d08f48e58baa218121611ad4e4331ea
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <herukiraj@gmail.com>
2024-12-26 20:58:22 +0530

    Update ~reaction.py

--- nexichat/mplugin/~reaction.py
+++ nexichat/mplugin/~reaction.py
@@ -1,16 +1,20 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from nexichat import nexichat
+import random
 
+EMOJIS = ["🥰", "❤️", "😁", "💋", "😱", "🤣", "😘", "❤️‍🔥", "👌", "🫡", "😍"]
 
-# List of emojis for reactions
-# SUPPORTED_EMOJIS = ["👍", "❤️", "😂", "🔥", "👏"]
-
-# Function to react to incoming messages
-@nexichat.on_message(filters.incoming)
+@Client.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
     try:
-            await message.react("💥","💗")  # React to the same message with each emoji
-            await asyncio.sleep(0.5)  # Delay between reactions to avoid rate limiting
+        random_emoji = random.choice(EMOJIS)
+        await message.react(random_emoji)
+        await asyncio.sleep(0.5)
     except Exception as e:
         print(f"Failed to react to message: {e}")
+
+
+
+
+
+
